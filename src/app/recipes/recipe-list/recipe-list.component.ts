88c6ba965a535bd02c48bceb13dfe2b7917e2dd3
Tom Bluher <tbluher14@gmail.com>
2023-01-24 10:25:12 -0700

    added a test recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes = []
-  
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply a test', 'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_1280,c_limit/RoastChicken_RECIPE_080420_37993.jpg')
+  ]
 }
